7b3062c5d162ec1bc60ad57701c984117afe4c14
yl <2201441955@qq.com>
2024-03-01 13:53:09 +0800

    Add build-all.sh

---
+++ build-all.sh
@@ -0,0 +1,6 @@
+./build.sh test_skiplist.mojo -o test_skillist
+./build.sh test_all.mojo -o test_all
+./build.sh test_trade.mojo -o test_trade
+./build.sh test_bybit.mojo -o test_bybit
+./build.sh test_binance.mojo -o test_binance
+./build.sh main.mojo -o moxt
